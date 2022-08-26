6c5a8b3ee8cd08d472153f8ba5e0b41de5c371f8
zirtaeb04 <beatriz.zpta@gmail.com>
2022-08-26 16:30:14 -0500

    Este es un ejemplo de la actualizacion realizada en (EjemploClasemaster)

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+//Este comentario es para ver si mi git esta funcionando correctamente
+
 }
